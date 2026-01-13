edfaec2a980754ac3478c0e62d660814d2231cca
dineshsrisai <dineshsrisai1@gmail.com>
2026-01-13 22:35:54 +0530

    creating swiggy clone

--- app.js
+++ app.js
@@ -1,16 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const parent = React.createElement("h1", { id: "h1tag" }, "50 days challenge");
-
-const Heading = () => {
+const Header = () => {
   return (
-    <div id="container">
-      <h1 className="heading">50 days challenge</h1>
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://logos-world.net/wp-content/uploads/2020/11/Swiggy-Emblem.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const ResCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-img"
+        alt="res-img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/bb48c990-87ae-4884-83d8-03d73f3cd2a7_10576.JPG"
+      />
+      <h3>Pizza Hut</h3>
+      <h4>4.2</h4>
+      <h5>Pizzas</h5>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <ResCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      {/* <Footer /> */}
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading />);
+root.render(<AppLayout />);
